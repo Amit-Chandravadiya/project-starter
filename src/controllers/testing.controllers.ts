390541acd29d07747bf5dd97b121b8b7d084b551
Amit-Chandravadiya <amitckn7@gmail.com>
2024-09-22 00:15:24 +0530

    Modified contorller for validations and respons sending

--- src/controllers/testing.controllers.ts
+++ src/controllers/testing.controllers.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import ApiResponse from "../utils/ApiResponse";
 import { testApiValidator } from "../validators/test.validator";
-import { ApiError } from "../utils/ApiError";
 
 const apiCheck = async (req:Request, res:Response): Promise<Response> => {
     // validating incoming request body
@@ -11,7 +10,7 @@ const apiCheck = async (req:Request, res:Response): Promise<Response> => {
     if(error!= null)
     {
       // throw new ApiError(400,"There is a problem with the validation of the request body !",)
-      return res.status(400).json(new ApiResponse(400,"Error while validating data !",error))
+      return res.status(400).json(new ApiResponse(400,"Error while validating data !",error.details[0].message))
     }
     
     // If no error is produced return the data as it is as this is just for the testing purpose only
